da4b982124d58520cc97cf32c66578d2a93e9e12
Dennis Prudlo <dennis.prudlo@outlook.com>
2019-07-07 13:04:18 +0200

    Add app icon

---
+++ HTW Mate/Constants/HWIcons.swift
@@ -0,0 +1,17 @@
+//
+//  HWIcons.swift
+//  HTW Mate
+//
+//  Created by Dennis Prudlo on 7/7/19.
+//  Copyright © 2019 Dennis Prudlo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct HWIcons {
+
+    static let studentCard = UIImage(named: "student-card")
+    static let settings = UIImage(named: "settings")
+
+}
