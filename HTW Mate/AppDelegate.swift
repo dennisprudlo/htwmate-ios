1a5aa2caefdbe325d173613a86091d77172a877c
Dennis Prudlo <mail@dennisprudlo.com>
2019-11-17 02:26:32 +0100

    Remove the appearance manager class and handle the appearance protocol in the app delegate

--- HTW Mate/AppDelegate.swift
+++ HTW Mate/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
 
         //
         // Update app appearance styles
-        AppearanceManager.updateControlsAppearance()
+        updateControlsAppearance()
 
         //
         // Preload app data
@@ -30,6 +30,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
 
         return true
     }
+	
+	// MARK: Configure components using the appearance protocol
+	
+	private func updateControlsAppearance() -> Void {
+	   let switchAppearance = UISwitch.appearance()
+	   switchAppearance.onTintColor = HWColors.StyleGuide.primaryGreen
+
+	   let barButtonItems = UIBarButtonItem.appearance(whenContainedInInstancesOf: [HWNavigationController.self])
+	   barButtonItems.tintColor = HWColors.StyleGuide.primaryGreen
+
+	   let greenController = UIBarButtonItem.appearance(whenContainedInInstancesOf: [PDFViewController.self])
+	   greenController.tintColor = HWColors.contentBackground
+	   
+	   let tabBarItemAppearance = UITabBarItem.appearance()
+	   tabBarItemAppearance.setTitleTextAttributes([
+		   .font: Font.shared.get(fontSize: .small)
+	   ], for: .normal)
+   }
 
 	// MARK: User Notification Center
 
